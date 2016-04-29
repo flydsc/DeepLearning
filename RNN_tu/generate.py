a88320f9610e57ca6dea333412869aaee1cb8596
dsc.rMBP13 <dongshichao@gmail.com>
2016-04-29 23:19:26 +0800

    first

---
+++ RNN_tu/generate.py
@@ -0,0 +1,34 @@
+from utils import load_model_parameters_theano, save_model_parameters_theano
+# from train import *
+
+model = RNNTheano(vocabulary_size, hidden_dim=50)
+# losses = train_with_sgd(model, X_train, y_train, nepoch=50)
+# save_model_parameters_theano('./data/trained-model-theano.npz', model)
+load_model_parameters_theano('./data/trained-model-theano.npz', model)
+
+
+
+def generate_sentence(model):
+    # We start the sentence with the start token
+    new_sentence = [word_to_index[sentence_start_token]]
+    # Repeat until we get an end token
+    while not new_sentence[-1] == word_to_index[sentence_end_token]:
+        next_word_probs = model.forward_propagation(new_sentence)
+        sampled_word = word_to_index[unknown_token]
+        # We don't want to sample unknown words
+        while sampled_word == word_to_index[unknown_token]:
+            samples = np.random.multinomial(1, next_word_probs[-1])
+            sampled_word = np.argmax(samples)
+        new_sentence.append(sampled_word)
+    sentence_str = [index_to_word[x] for x in new_sentence[1:-1]]
+    return sentence_str
+
+num_sentences = 10
+senten_min_length = 7
+
+for i in range(num_sentences):
+    sent = []
+    # We want long sentences, not sentences with one or two words
+    while len(sent) < senten_min_length:
+        sent = generate_sentence(model)
+    print " ".join(sent)
